414ae2d8b58ae739044848a9a324ffed55115efd
buglimar <buglimarcoelhogomes@gmail.com>
2021-05-21 17:35:10 -0300

    upadat

--- fundamentos/tipagemfraca.js
+++ fundamentos/tipagemfraca.js
@@ -5,9 +5,9 @@ console.log(typeof qualquer)
 qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
-// evitar nome genericos e siglas
-lef valor = ''
-lef número = 1
+
+lef valor = '' // evitar nome genericos e siglas
+lef numero = 1
 lef pqp = false // produto quimico perigoso... kkk
 
 
