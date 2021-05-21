9b8b8cda3fba7a0cab80c889e471163c74e3a129
buglimar <buglimarcoelhogomes@gmail.com>
2021-05-21 18:20:32 -0300

    update

---
+++ fundamentos/strings.js
@@ -0,0 +1,24 @@
+const escola = "Cod3r"
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+console.log(escola.substring(1))
+console.log(escola.substring(0, 4))
+console.log('escola '.concat(escola).concat("!@"))
+console.log(escola.replace(3, 'e'))
+console.log(escola.repeat(3))
+console.log('Ana,Maria,Pedro'.split(','))
+console.log('escola ' + escola + "??") // tipo de concat com +
+
+
+
+
+
+
+
+
+
+
+
+
