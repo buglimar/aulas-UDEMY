27d9907f1963c2736a406889ad1cfd1ce5b84734
buglimar <buglimarcoelhogomes@gmail.com>
2021-05-21 19:11:27 -0300

    Update

--- fundamentos/tipagemfraca.js
+++ fundamentos/tipagemfraca.js
@@ -5,10 +5,13 @@ console.log(typeof qualquer)
 qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
+/* evitar nome genericos e siglas
 
-lef valor = '' // evitar nome genericos e siglas
+lef valor = '' 
 lef numero = 1
 lef pqp = false // produto quimico perigoso... kkk
+
+*/
 
 
 
